03461aa18d52a9ef5b2009655e7c376293cf5d00
Hitharlin <98156708+Hitharlin@users.noreply.github.com>
2022-08-25 09:57:21 +0400

    Update DataTable.js

--- assets/js/DataTable.js
+++ assets/js/DataTable.js
@@ -1,28 +1,41 @@
   class DataTable {
     constructor(columns = [], data = [], {
         perPageLimit = 5, 
-        rowClassName = 'data-row',
-        cellClassName = 'data-cell',
-        tableClassName = 'data-table',
-        arrowIcon = 'arrow-icon',
-        blockOption = 'block-option',
-    }) {
-      this.columns = columns;
-      this.data = data;
-      this.perPageLimit = perPageLimit;
-      this.rowClassName = rowClassName;
-      this.cellClassName = cellClassName;
-      this.tableClassName = tableClassName;
-      this.arrowIcon = arrowIcon;
-      this.blockOption = blockOption;   
+        rowClassName = '',
+        tableClassName = '',
+        thClassName = '',
+        tdClassName = '',
+        paginationBoxClassName = '',
+        pageItemClassName = '',
+        searchFieldClassName = '',
+        labelClassName = '',
+        checkAllClassName = '',
+        pageContainerClassName = '',
+        pageActiveClassName = '',
+    })
+    {
+        this.columns = columns;
+        this.data = data;
+        this.perPageLimit = perPageLimit;
+        this.rowClassName = `data__row ${rowClassName}`; 
+        this.tableClassName = `datatable ${tableClassName}`;
+        this.checkedInput = [];
+        this.thClassName = `th__item ${thClassName}`;
+        this.tdClassName = `td__item ${tdClassName}`;
+        this.paginationBoxClassName = `pagination__box ${paginationBoxClassName}`;
+        this.pageItemClassName = `page__item ${pageItemClassName}`;
+        this.searchFieldClassName = `search__field ${searchFieldClassName}`;
+        this.labelClassName = `label ${labelClassName}`;
+        this.checkAllClassName = `check__all ${checkAllClassName}`;
+        this.pageContainerClassName =`page__container ${pageContainerClassName}`;
+        this.pageActiveClassName = `page__item--active ${pageActiveClassName}`;
     }
 
     createTable($dataTableContainer) {
         const $table = document.createElement('table');
-        $table.classList.add(this.tableClassName);  //////նոր կլաս  (ռեֆակտրինգ)
+        $table.setAttribute('class', this.tableClassName);
         this.table = $table;
         $dataTableContainer.appendChild(this.table);
-
         this.createThead();
         this.createTbody();
         this.renderData();
@@ -34,42 +47,81 @@
     createThead() {
         const $thead = document.createElement('thead');
         const $tr = document.createElement('tr');
-        const $thCheck = document.createElement('th')
+        const $thCheck = document.createElement('th');
+        $thCheck.setAttribute('class', this.thClassName);
         const $checkinput = document.createElement('input');
         $thCheck.appendChild($checkinput);
         $checkinput.setAttribute('type', 'checkbox');
-        $checkinput.addEventListener('change', (event) =>{
-                for(let i = 0; i < document.querySelectorAll('tbody tr').length; i++){
+        $checkinput.setAttribute('class', this.checkAllClassName);
+
+        $checkinput.addEventListener('change', (event) => {
+            for(let i = 0; i < document.querySelectorAll('tbody tr').length; i++){
                     document.querySelectorAll('tbody tr input')[i].checked = $checkinput.checked ;
                 }                    
             }
-        )
+        );
         
         $tr.appendChild($thCheck)
-    
+
         this.columns.forEach((item) => {
             const $th = document.createElement('th');
+            $th.setAttribute('class', this.thClassName);
             $th.dataset.filterBy = '';
             $th.innerHTML = item;
 
-            $th.addEventListener('click', (event) =>{
+            $th.addEventListener('click', (event) => {
                 let $clickedItem = event.target;
                 if( $clickedItem.dataset.filterBy != (`${item}-asc`)) {
                     $clickedItem.dataset.filterBy = (`${item}-asc`);
                 } else{
                     $clickedItem.dataset.filterBy = (`${item}-desc`);
                 };
-                this.testSort($clickedItem.dataset.filterBy);
+                this.createSorting($clickedItem.dataset.filterBy); 
             })
        
             $tr.appendChild($th);  
         });
-            const $dltTh = document.createElement('th');
-            $tr.appendChild($dltTh);
-            $thead.appendChild($tr);
-            this.table.appendChild($thead);
-            // this.table.appendChild($checkThead);
 
+            const $dltTh = document.createElement('th');
+            $dltTh.setAttribute('class', this.thClassName);
+            const $tdBtn = document.createElement('button');
+            const $iIcon = document.createElement('i');
+            $iIcon.classList.add('fa');
+            $iIcon.classList.add('fa-trash');
+            $tdBtn.appendChild($iIcon);
+            $dltTh.appendChild($tdBtn)
+            $tr.appendChild($dltTh);
+
+            $iIcon.addEventListener('click', (event) =>{
+                if($checkinput.checked){
+                    for(let i = 0; i < this.perPageLimit; i++){
+                        this.checkedInput.push(document.querySelectorAll('tbody tr input')[i].dataset.currentId);
+                    }
+
+                    this.data = this.data.filter((item) => !this.checkedInput.includes((item.id).toString()))
+                    this.table.querySelector('tbody').innerHTML = null;
+                    document.querySelector('.page__container').innerHTML = null;
+                    $checkinput.checked = false;
+                    this.createPagination();           
+                    this.renderData();
+                } else {
+                    for(let i = 0; i < this.perPageLimit; i++){
+                        if(document.querySelectorAll('tbody tr input')[i].checked){
+                            this.checkedInput.push(document.querySelectorAll('tbody tr input')[i].dataset.currentId);
+                        }
+                    }
+
+                    this.data = this.data.filter((item) => !this.checkedInput.includes((item.id).toString()))
+                    this.table.querySelector('tbody').innerHTML = null;
+                    document.querySelector('.page__container').innerHTML = null;
+                    $checkinput.checked = false;
+                    this.createPagination();           
+                    this.renderData();
+                }
+            })
+
+            this.table.appendChild($thead);
+            $thead.appendChild($tr);
     }
     
     createTbody() {
@@ -82,50 +134,54 @@
     }
         
     createPagination(){
+        if(this.data.length > this.perPageLimit) {
+            const $pageInfo = document.createElement('div');
+            document.querySelector('body').appendChild($pageInfo)
+            $pageInfo.setAttribute('class', this.pageContainerClassName)
+            const paginationBox = document.createElement('div');
+            const pageNumber = Math.ceil(this.data.length / this.perPageLimit);
+            paginationBox.setAttribute('class', this.paginationBoxClassName);
+    
+            for(let i = 1; i <= pageNumber; i++){
+                const $pageButton = document.createElement('button');
+                $pageButton.setAttribute('class', this.pageItemClassName);
+                $pageButton.innerHTML = i;
+                paginationBox.appendChild($pageButton);
 
-            if(this.data.length > this.perPageLimit) {
-                const paginationBox = document.createElement('div');
-                const pageNumber = Math.ceil(this.data.length / this.perPageLimit);
-                paginationBox.classList.add('pagination-box')
-        
-                for(let i = 1; i <= pageNumber; i++){
-                    const $pageButton = document.createElement('button');
-                    $pageButton.classList.add('page-item');
-                    $pageButton.innerHTML = i;
-                    paginationBox.appendChild($pageButton);
-
-                    if( i == 1 ){
-                        $pageButton.classList.add('page-active');
-                    }
+                if( i == 1 ){
+                    $pageButton.classList.add('page__item--active');
                 }
-        
-                document.querySelector('.data-table-container').appendChild(paginationBox);
-                document.querySelector('.page-info').appendChild(paginationBox);
-                const dataTable = this;
-
-                document.querySelectorAll('.page-item').forEach( (eachItem) => { 
-                    eachItem.addEventListener('click', function(){
-                    if(document.querySelector('.page-active')){
-                        document.querySelector('.page-active').classList.remove('page-active');
-                    }
-
-                    this.classList.add('page-active');
-                    let start = dataTable.perPageLimit * parseInt(this.innerHTML - 1);
-                    let end = dataTable.perPageLimit + start
-                    document.getElementsByTagName('tbody')[0].replaceChildren(); ////// էս փոխեմ նենց, որ this-ով ստանամ, որ ուրիշ թբոդիի չկպնեմ։
-                    dataTable.displayList(start, end);
-                    }   
-                    ) 
-                }) 
             }
+    
+            document.querySelector('.data-table-container').appendChild(paginationBox);
+            document.querySelector('.page__container').appendChild(paginationBox);
+            const dataTable = this;
+
+            document.querySelectorAll('.page__item').forEach((eachItem) => { 
+                eachItem.addEventListener('click', function(){
+                if(document.querySelector('.page__item--active')){
+                    document.querySelector('.page__item--active').classList.remove('page__item--active');
+                }
+
+                this.classList.add('page-active');
+                let start = dataTable.perPageLimit * parseInt(this.innerHTML - 1);
+                let end = dataTable.perPageLimit + start
+                document.querySelector('tbody').replaceChildren();
+                dataTable.displayList(start, end);
+                }) 
+            }) 
+        }
     }
 
     createInput() {
+        const $tableOptions = document.createElement('div')
+        $tableOptions.classList.add('table-options');
+        document.querySelector('body').appendChild($tableOptions);
         const $label = document.createElement('label');
+        $label.setAttribute('class', this.labelClassName)
         const $select = document.createElement('select');
         const dataTable = this;
-    
-        
+
         const $option1 = document.createElement('option');
         $option1.innerHTML = 5;
         const $option2 = document.createElement('option');
@@ -142,9 +198,10 @@
     
         $label.appendChild($select);
         document.querySelector('.table-options').appendChild($label);
+
         $select.addEventListener('change', (event) => {
             this.table.querySelector('tbody').innerHTML = null;    
-            document.querySelector('.page-info').innerHTML = null;
+            document.querySelector('.page__container').innerHTML = null;
             dataTable.perPageLimit = +event.target.value;
             dataTable.createPagination();
             dataTable.renderData();
@@ -152,40 +209,43 @@
     }
     
     displayList(startCount, endCount) {
-
         const eachPageItems = this.data.slice(startCount, endCount);
-        
         eachPageItems.forEach((item) => {
             const $tr = document.createElement('tr');
+            $tr.setAttribute('class', this.rowClassName);
             const $checkTd = document.createElement('td');
+            $checkTd.setAttribute('class', this.tdClassName);
             const $checkInput = document.createElement('input')
             $checkInput.setAttribute('type', 'checkbox');
             $checkTd.appendChild($checkInput);
             $tr.appendChild($checkTd);
-            $checkInput.classList.add('checked')
-            
+            $checkInput.dataset.currentId = item.id;
 
             for(const key in item) {
                 const $td = document.createElement('td');
                 $td.innerHTML = item[key];
                 $tr.appendChild($td);
+                $td.setAttribute('class', this.tdClassName);
             }
 
             $checkInput.addEventListener('change',(event) => {
+                item.id == event.target.dataset.currentId
+                this.checkedInput.push(item.id)
+                const $checkedCount = document.querySelectorAll('tbody tr input:checked').length;
 
-                for(let i = 0; i<document.querySelectorAll('tbody tr').length; i++){
-                    const $checkedCount = document.querySelectorAll('tbody tr input:checked').length;
-                    const $inputCount = document.querySelectorAll('tbody tr input').length
-                    
-                    if($checkedCount == $inputCount){
-                        document.querySelector('thead tr input').checked = true
+                for(let i = 0; i < document.querySelectorAll('tbody tr').length; i++){
+                
+                    if($checkedCount == this.perPageLimit){
+                        document.querySelector('thead tr input').checked = true;
+
                     } else {
-                        document.querySelector('thead tr input').checked = false
+                        document.querySelector('thead tr input').checked = false;
                     }
                 }
             })
 
             const $deleteTd = document.createElement('td');
+            $deleteTd.setAttribute('class', this.tdClassName);
             const $tdBtn = document.createElement('button');
             const $iIcon = document.createElement('i');
             $iIcon.classList.add('fa');
@@ -194,30 +254,25 @@
             $tdBtn.appendChild($iIcon)
 
             $iIcon.addEventListener('click', (event) => {
-                const indexOfObject = this.data.findIndex(item => {
-                    return item.id == event.target.dataset.currentRowId;
-                });
+                this.data = this.data.filter((item) => item.id != event.target.dataset.currentRowId)
 
-                this.data.splice(indexOfObject, 1);
+                if(document.querySelector('.check__all').checked){
+                    this.data = this.data.filter((item) => item.id != document.querySelector('.check__all'))
+                }
+
                 this.table.querySelector('tbody').innerHTML = null;
-                document.querySelector('.page-info').innerHTML = null;
+                document.querySelector('.page__container').innerHTML = null;
                 this.createPagination();           
                 this.renderData();
             })
 
             $deleteTd.appendChild($tdBtn);
             $tr.appendChild($deleteTd);
-
             document.getElementsByTagName('tbody')[0].appendChild($tr);
         })
     }
 
-    createCheck() {
-        
-    }
-
-    testSort(currentElement) {
-    
+    createSorting(currentElement) {
         this.data.sort((a, b) => {
             if(currentElement == 'id-asc'){
                 return a.id - b.id
@@ -229,14 +284,12 @@
                 } else if(a.name < b.name){
                     return 1
                 }
-                return 0
             } else if(currentElement == 'name-asc'){
                 if(a.name > b.name){
                     return 1
                 } else if(a.name < b.name){
                     return -1
                 }
-                return 0
             } else if(currentElement == 'age-asc'){
                 return a.age - b.age
             } else {
@@ -244,58 +297,50 @@
             }
         }) 
         this.table.querySelector('tbody').innerHTML = null; 
-        document.querySelector('.page-info').innerHTML = null; 
+        document.querySelector('.page__container').innerHTML = null; 
         this.renderData();
         this.createPagination();
     }
 
     createSearchField(){
         const $input = document.createElement('input');
+        $input.setAttribute('class', this.searchFieldClassName)
         $input.setAttribute("type", "search");
         $input.placeholder = 'search';
-        document.querySelector('body').appendChild($input);
-        
+        document.querySelector('.data-table-container').prepend($input);
+        const test = this.data
+
         $input.onchange = (event) => {
             const searchResult = [];
             let noResult = false;
+            
             this.data.map((item) => {
                 for(const key in item) {
                     if(item[key].toString().toLowerCase().startsWith($input.value.toLowerCase())){
                         searchResult.push(item);
                   }
                 }
-                document.querySelector('tbody').innerHTML = null;
             })
 
+            document.querySelector('tbody').innerHTML = null;
+
             if(searchResult.length > 0) {
-                searchResult.forEach((item, index) => {
-                    const $tr = document.createElement('tr');
-                      
-                    for(const key in item) {
-                        const $td = document.createElement('td');
-                        $td.innerHTML = item[key];
-                        $tr.appendChild($td);
-                      }
-                               
-                    document.getElementsByTagName('tbody')[0].appendChild($tr);
-                  })
-                } else {
-                    const $tr = document.createElement('tr');
-                    const $noResultTd = document.createElement('td');
-                    $noResultTd.colSpan = '4';
-                    $noResultTd.innerHTML = 'No Result';
-                    $tr.appendChild($noResultTd);
-                    document.querySelector('tbody').appendChild($tr);
-                }
+                this.data = searchResult;
+                this.table.querySelector('tbody').innerHTML = null;
+                document.querySelector('.page__container').innerHTML = null;
+                this.createPagination();           
+                this.renderData();
+                this.data = test;
+            } else {
+                const $tr = document.createElement('tr');
+                const $noResultTd = document.createElement('td');
+                $noResultTd.colSpan = this.columns + 2;
+                $noResultTd.innerHTML = 'No Result';
+                $tr.appendChild($noResultTd);
+                document.querySelector('tbody').appendChild($tr);
+            }
         }
     } 
 }
 
-
-
-
-
-
-
 export default DataTable
-
